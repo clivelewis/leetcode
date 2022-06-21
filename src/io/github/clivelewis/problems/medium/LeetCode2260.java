c30a6c9e56e73a3abeec5c2acecaac7982f59eaf
CliveLewis <viktorsms95@gmail.com>
2022-06-21 17:00:32 +0200

    Description for 2260

--- src/io/github/clivelewis/problems/medium/LeetCode2260.java
+++ src/io/github/clivelewis/problems/medium/LeetCode2260.java
@@ -3,7 +3,15 @@ package io.github.clivelewis.problems.medium;
 import io.github.clivelewis.testutils.TestExecutor;
 
 import java.util.HashMap;
+/*
+2260. Minimum Consecutive Cards to Pick Up
 
+You are given an integer array cards where cards[i] represents the value of the ith card.
+A pair of cards are matching if the cards have the same value.
+
+Return the minimum number of consecutive cards you have to pick up to have a pair of matching cards among the picked cards.
+If it is impossible to have matching cards, return -1.
+ */
 public class LeetCode2260 {
 	public static void main(String[] args) {
 		TestExecutor.execute(minimumCardPickup(new int[]{3,4,2,3,4,7}), 4);
